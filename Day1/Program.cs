ab1b813a32c01fd1b4b14909b842b7abbf4ee92f
Jesse Quijano <jesse@quijano.net>
2021-12-04 19:42:54 -0800

    part a/b answers for day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Advent2021.Helpers;
 
 namespace Day_1
@@ -14,8 +16,40 @@ namespace Day_1
 
             for (int i=1; i < depths.Count; i++)
             {
-                Console.WriteLine(depths[i]);
+                if (depths[i] > depths[i-1])
+                {
+                    higher++;
+                }
+                else if (depths[i] < depths[i-1])
+                {
+                    lower++;
+                }
             }
+
+            Console.WriteLine(higher); // answer A
+
+            List<int> sums = new();
+            higher = 0;
+            lower = 0;
+
+            for (int i=0; i < depths.Count - 2; i++)
+            {
+                sums.Add(depths.GetRange(i, 3).Sum());
+            }
+
+            for (int i=1; i < sums.Count; i++)
+            {
+                if (sums[i] > sums[i-1])
+                {
+                    higher++;
+                }
+                else if (sums[i] < sums[i-1])
+                {
+                    lower++;
+                }
+            }
+            
+            Console.WriteLine(higher); // answer B
         }
     }
 }
