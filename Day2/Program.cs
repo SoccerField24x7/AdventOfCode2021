39283a96f6041d78f78758d7044010343ddb2e2d
Jesse Quijano <jesse@quijano.net>
2021-12-10 12:19:23 -0800

    part I solution

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Advent2021.Helpers;
+using Advent2021.Constants;
 
 namespace Advent2021.Day2
 {
@@ -7,8 +8,32 @@ namespace Advent2021.Day2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var instructions = FileHelper.GetFileContents<string>("data/input.txt");
+
             var shipPosition = new ManhattanLocation();
+
+            foreach (string instruction in instructions)
+            {
+                string[] parts = instruction.Split(" ");
+
+                switch (parts[0])
+                {
+                    case "up":
+                        shipPosition = ShipMover.MoveForward(shipPosition, Constants.Direction.UP, int.Parse(parts[1]));
+                        break;
+                    case "down":
+                        shipPosition = ShipMover.MoveForward(shipPosition, Constants.Direction.DOWN, int.Parse(parts[1]));
+                        break;
+                    case "forward":
+                        shipPosition = ShipMover.MoveForward(shipPosition, Constants.Direction.FORWARD, int.Parse(parts[1]));
+                        break;
+                    default:
+                        throw new InvalidOperationException("The requested direction was invalid.");
+                }
+            }
+
+            Console.WriteLine(shipPosition.HorizontalPosition * shipPosition.VerticalPosition);
+
         }
     }
 }
