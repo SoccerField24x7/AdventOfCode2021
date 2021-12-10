01dfcdf21fb166da9348dc837dcb11474720dfae
Jesse Quijano <jesse@quijano.net>
2021-12-09 22:25:00 -0800

    add constants for new directions

--- Constants/Directions.cs
+++ Constants/Directions.cs
@@ -8,5 +8,13 @@ namespace Advent2021.Constants
         public const int EAST = 90;
         public const int SOUTH = 180;
         public const int WEST = 270;
+
+        public const int UP = 0;
+        
+        public const int DOWN = 180;
+
+        public const int FORWARD = 90;
+
+        public const int BACKWARD = 270;
     }
 }
